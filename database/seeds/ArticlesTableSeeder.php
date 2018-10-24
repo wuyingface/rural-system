cde19390a0f673d359582738a22c96dc679e2c8d
wuyingface <xuyingface@163.com>
2018-10-24 09:49:20 +0000

    生成虚拟用户和文章

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -2,18 +2,38 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Article;
+use App\Models\User;
+use App\Models\ArticleCategory;
 
 class ArticlesTableSeeder extends Seeder
 {
     public function run()
     {
-        $articles = factory(Article::class)->times(50)->make()->each(function ($article, $index) {
-            if ($index == 0) {
-                // $article->field = 'value';
-            }
+        // 所有用户 ID 数组，如：[1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组，如：[1,2,3,4]
+        $category_ids = ArticleCategory::all()->pluck('id')->toArray();
+
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $articles = factory(Article::class)
+                        ->times(100)
+                        ->make()
+                        ->each(function ($article, $index)
+                            use ($user_ids, $category_ids, $faker)
+        {
+            // 从用户 ID 数组中随机取出一个并赋值
+            $article->user_id = $faker->randomElement($user_ids);
+
+            // 话题分类，同上
+            $article->category_id = $faker->randomElement($category_ids);
         });
 
+        // 将数据集合转换为数组，并插入到数据库中
         Article::insert($articles->toArray());
+
     }
 
 }
