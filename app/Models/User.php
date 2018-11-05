9c97e1153f5bc610ba43b71eb52ca28de2fc19b8
Tony <xuyingface@163.com>
2018-11-05 15:14:04 +0800

    初步完成后台的用户管理

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,31 @@ class User extends Authenticatable
         $this->unreadNotifications->markAsRead();
     }
 
+    //针对密码的模型修改器（后台管理入库之前持久性更新）
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    //针对头像的模型修改器（后台管理入库之前持久性更新）
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! starts_with($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
+
+
 }
