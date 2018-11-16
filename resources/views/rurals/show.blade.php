eca36855f285c2d84f4369981d72a5af5064501a
Denton123 <1198503549@qq.com>
2018-11-16 16:10:06 +0800

    修改乡村文章展示页

--- resources/views/rurals/show.blade.php
+++ resources/views/rurals/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
   <div class="container row">
-    <div class="col-md-9 col-md-push-3">
+    <div class="col-md-9 col-md-push-3 rightPart">
         <div class="introduct_header">
             <div class="jumbotron">
                 <h1>{{$rural -> name}}</h1>
@@ -37,40 +37,120 @@
             </ul>
         </div>
         <!-- 文章列表 -->
-        <div class="container">
+        <div class="container articleList">
             <p class="lead" id="detailTitle">人文</p>
             <p>
                 something detail...
             </p>
         </div>
         <!-- 新增文章表单 -->
-        <div>
-            <iframe id="createArticle" name="createArticle" frameborder="0"></iframe>
+        <div class="createArticleMark">
+            <form class="createArticle" action="{{ route('articles.store') }}" method="POST" accept-charset="UTF-8">   
+                <h4 style="font-weight: bold;margin-bottom: 18px;">发表关于{{$rural->name}}的文章</h4>   
+                <span class="glyphicon glyphicon-remove closeBtn"></span>
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="rural_id" value="{{ $rural->id }}">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="title" placeholder="请填写标题" required/>
+                </div>
+
+                <div class="form-group">
+                    <select class="form-control" name="category_id" required>
+                        <option value="" hidden disabled>请选择分类</option>
+                            @foreach ($articleCategories as $articleCategory)
+                                <option value="{{ $articleCategory->id }}">{{ $articleCategory->name }}</option>
+                            @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少十三字符的内容。"></textarea>
+                </div>
+
+                <div class="form-group" style="display: none;">
+                    <input name="map" id="coordinate">
+                    
+                </div>
+                    
+                <div class="form-group">
+                    <input type="text" name="location" class="form-control" id="position" placeholder="请点击地图选取位置" style="width: 500px;display: inline-block;" readonly="readonly">
+                    <a href="javascript:void(0);" onclick="hasMap()" id="hasMap">显示地图</a>
+                </div>
+
+                <div class="form-group">
+                    <input type="text" name="location_name" class="form-control" style="display: none;margin-bottom: 5px;" id="userDefined" placeholder="自定义地址名称" />
+                    <div style="display: none;" id="mapWrap">
+                        <input class="form-control" type="text" placeholder="搜索位置" id="searchId">
+                        <div id="map" style="height: 500px;"></div>
+                    </div>
+                </div>
+
+                <div class="well well-sm">
+                    <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
+                </div>
+            </form>
         </div>
     </div>
-    <div class="col-md-3 col-md-pull-9">
+    <div class="col-md-3 col-md-pull-9 leftPart">
         <ul class="nav nav-pills nav-stacked">
-            <?php
-                $navs = ['概况', '乡村发展', '民风民俗', '文化建设', '吃喝玩乐', '随想随写']
-                ?>
-            @foreach ($navs as $nav)
-            <li class="nav_li"><a href="#">{{$nav}}</a></li>
+                <li class="nav_li"><a href="#">概况</a></li>
+            @foreach ($articleCategories as $articleCategory)
+                <li class="nav_li"><a href="#">{{$articleCategory->name}}</a></li>
             @endforeach
+                <li class="nav_li hasCreateArticle"><a href="#">新建乡村文章</a></li>
         </ul>
-            <a class="btn btn-primary" href="{{ route('articles.create') }}" target="createArticle">新增文章</a>
     </div>
   </div>
 @endsection
 @section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('js/editor/css/simditor.css') }}">
     <style>
-        #createArticle {
+        .createArticle {
+            width: 700px;
+            height: 700px;
+            border: 1px solid #ccd0d2;
+            position: absolute;
+            padding: 45px;
+            border-radius: 5px;
+            z-index: 9999;
+            top: 50%;
+            left: 50%;
+            margin-top: -350px;
+            margin-left: -350px;
+            background: #fff;
+            box-shadow: 1px 1px 50px rgba(0,0,0,.3);
+            border-radius: 2px;
+            overflow: scroll;
+        }
+        .createArticleMark{
+            position: fixed;
             width: 100%;
-            height: 600px;
+            height: 100%;
+            background: rgba(0,0,0,.5);
+            top: 0;
+            left: 0;
+            z-index: 2000;
+            display: none;
+        }
+        .closeBtn{
+            position: absolute;
+            width: 30px;
+            height: 30px;
+            right: 0px;
+            top: 10px;
+            cursor: pointer;
         }
     </style>
 @stop
 @section('scripts')
+<script type="text/javascript"  src="{{ asset('js/editor/js/module.js') }}"></script>
+<script type="text/javascript"  src="{{ asset('js/editor/js/hotkeys.js') }}"></script>
+<script type="text/javascript"  src="{{ asset('js/editor/js/uploader.js') }}"></script>
+<script type="text/javascript"  src="{{ asset('js/editor/js/simditor.js') }}"></script>
+<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=7OOvPVfFtq8vZEUZn1Zv5Q7W4ndE4g0H"></script>
+<script type="text/javascript"  src="{{ asset('js/commonJs/map.js') }}"></script>
 <script type="text/javascript">
+
     $(function () {
         // body...
         $('.nav_li:first-child').addClass('active')
@@ -87,6 +167,24 @@
                 $('#introduct_list').hide()
                 $('#detailTitle').html($(this).find('a').html())
             })
+        })
+        // 文本编辑器
+        var editor = new Simditor({
+            textarea: $('#editor'),
+            upload: {
+                url: '{{ route('articles.upload_image') }}',
+                params: { _token: '{{ csrf_token() }}' },
+                fileKey: 'upload_file',
+                connectionCount: 3,
+                leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+            },
+            pasteImage: true,
+        });
+        $('.hasCreateArticle').click(function(){
+            $('.createArticleMark').show()
+        })
+        $('.closeBtn').click(function() {
+            $('.createArticleMark').hide()
         })
     })
 </script>
